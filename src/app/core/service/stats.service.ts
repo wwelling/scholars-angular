1cf8feaae2af55dbe84b3cc365ab45b2517b797a
William Welling <wwelling@tamu.edu>
2023-08-16 08:32:20 -0500

    Only collect states for platform browser (#272)

--- src/app/core/service/stats.service.ts
+++ src/app/core/service/stats.service.ts
@@ -1,19 +1,24 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppConfig, APP_CONFIG } from 'src/app/app.config';
 import { RestService } from './rest.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class StatsService {
 
-  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig, private rest: RestService) {
+  constructor(
+    @Inject(APP_CONFIG) private appConfig: AppConfig,
+    @Inject(PLATFORM_ID) private platformId: string,
+    private rest: RestService
+  ) {
 
   }
 
   public collect(queryParams: Params): Observable<any> {
     return new Observable((observer) => {
-      if (this.appConfig.collectSearchStats) {
+      if (isPlatformBrowser(this.platformId) && this.appConfig.collectSearchStats) {
         this.rest.get('https://php.library.tamu.edu/utilities/vivo_page_info.php').toPromise().then((data: any) => {
           data.queryParams = queryParams;
           this.rest.post('https://scholars.library.tamu.edu/vivo_editor/insert_stat.php', data, {
